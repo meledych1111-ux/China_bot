927c3466f8f07249bf7776313f2d47368701eedc
meledych1111-ux <meledych1111@gmail.com>
2026-01-11 22:23:56 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Отключаем ESLint при сборке
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  // Увеличиваем лимит размера тела для webhook
-  api: {
-    bodyParser: {
-      sizeLimit: '1mb',
-    },
   },
 };
 
